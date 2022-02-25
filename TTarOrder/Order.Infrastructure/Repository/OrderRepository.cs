d06f3c5154e596e6c89d5f9f0c876a1738a19959
dzgntutar <duzguntutar@gmail.com>
2022-02-25 18:15:36 +0300

    ddd

---
+++ TTarOrder/Order.Infrastructure/Repository/OrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Order.Application.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Infrastructure.Repository
+{
+    public  class OrderRepository : IOrderRepository
+    {
+    }
+}
